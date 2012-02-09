b63e4d9ec9892886e118cb3b73bcd0e2ec748abf
Chris Wilson <chris+github@qwirx.com>
2012-02-09 22:32:08 +0000

    Test that users are indexed.

--- tests.py
+++ tests.py
@@ -12,6 +12,25 @@ from django.template import Context
 import settings
 
 from binder.test_utils import AptivateEnhancedTestCase
+from binder.models import IntranetUser
+from search import SearchViewWithExtraFilters
+from intranet.search.search import SearchTable
 
 class SearchTest(AptivateEnhancedTestCase):
-    pass
+    fixtures = ['test_permissions', 'test_users']
+
+    def setUp(self):
+        super(SearchTest, self).setUp()
+        self.john = IntranetUser.objects.get(username='john')
+
+    def test_search_indexes_people(self):
+        response = self.client.get(reverse('search'), {'q': 'john'})
+        table = response.context['results_table']
+        self.assertIsInstance(table, SearchTable)
+        queryset = table.data.queryset
+        results = list(queryset)
+        self.assertEqual(1, len(results), "unexpected results in list: %s" %
+            results)
+        result = results[0]
+        self.assertEqual("binder.intranetuser.%s" % self.john.id, result.id)
+        

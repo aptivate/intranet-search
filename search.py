369bc3f39e8ae031dccc80238f2fb4bdd1b9f30a
Chris Wilson <chris+github@qwirx.com>
2012-04-03 14:54:10 +0100

    Add "document list" view using search backend.

--- search.py
+++ search.py
@@ -4,6 +4,7 @@ from django import forms
 from django.db import models as fields
 from django.forms import widgets
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.utils.safestring import mark_safe
 
 from haystack import connections, connection_router
@@ -212,11 +213,6 @@ class SearchFormWithJqueryLists(SearchFormWithAllFields):
             SelectMultipleWithJquery(html_name='id_models[]')
         self.fields['models'].widget.choices = self.fields['models'].choices
 
-from django.contrib.admin.views.main import ChangeList
-class SearchList(ChangeList):
-    def url_for_result(self, result):
-        return result.object.get_absolute_url()
-
 from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
 
 class SearchTable(tables.Table):
@@ -278,21 +274,15 @@ class SearchViewWithExtraFilters(SearchView):
     page_field = 'page'
     sort_by_field = 'sort'
 
-    from django.template import RequestContext
-    
     def __init__(self, template=None, load_all=True, 
-        form_class=SearchFormWithJqueryLists,
-        searchqueryset=None, context_class=RequestContext,
-        results_per_page=None):
+        form_class=SearchFormWithJqueryLists, searchqueryset=None, 
+        context_class=RequestContext, results_per_page=None):
         
         super(SearchViewWithExtraFilters, self).__init__(template, load_all,
             form_class, searchqueryset, context_class, results_per_page)
 
-    """
     def get_results(self):
-        import pdb; pdb.set_trace()
         return super(SearchViewWithExtraFilters, self).get_results()
-    """
      
     def create_response(self):
         if self.results is None:
@@ -340,3 +330,48 @@ class SearchViewWithExtraFilters(SearchView):
 
     # https://docs.djangoproject.com/en/dev/topics/class-based-views/
     __call__ = method_decorator(login_required)(SearchView.__call__)
+
+class DocumentListView(SearchViewWithExtraFilters):
+    """
+    This is a SearchView with a tweak: it pre-selects the Documents model
+    for restricted searches, which means that it returns all documents
+    when there is no query, instead of an empty list.
+    
+    This feels like it should be in the documents module, not here.
+    But there are good reasons to put it here:
+    
+    * It uses the search index to list the documents and retrieve
+    the fields, and it has a search form on the page, so it's not
+    easy to implement in the documents module, and would introduce
+    a cross-module dependency there.
+    
+    * Despite the name it doesn't actually depend on the Document
+    model in any way, so it doesn't introduce a cross-module dependency
+    if placed here.
+    
+    * It could be useful in other applications, at least as an example,
+    so I'm reluctant to make it a custom view hidden away in a client's
+    private intranet code.
+    """
+    
+    prefix = 'results_'
+    page_field = 'page'
+    sort_by_field = 'sort'
+
+    def build_form(self, form_kwargs=None):
+        data = None
+        kwargs = {
+            'load_all': self.load_all,
+        }
+        if form_kwargs:
+            kwargs.update(form_kwargs)
+
+        data = {'id_models[]': ['documents.document']}
+        
+        if len(self.request.GET):
+            data.update(self.request.GET)
+
+        if self.searchqueryset is not None:
+            kwargs['searchqueryset'] = self.searchqueryset
+
+        return self.form_class(data, **kwargs)
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
-from search import SearchViewWithExtraFilters
+from search import SearchViewWithExtraFilters, DocumentListView
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', SearchViewWithExtraFilters(), name='search'),
+    url(r'^documents', DocumentListView(), name='document_list')
 )
